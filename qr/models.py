9dc0b2c13bcf54878d71705d4b7c208f9b26049a
kecastro <keacastropo@unal.edu.co>
2018-05-16 10:44:11 -0500

    Creacion de modelos

--- qr/models.py
+++ qr/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+DOCUMENTOS = (
+    ("Cedula", "Cedula"),
+    ("Pasaporte", "Pasaporte")
+)
+
+class Clase(models.Model):
+    inicio = models.DateTimeField(default=datetime.now)
+    fin = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return "Inicio: " + str(self.inicio) + " - Fin: " + str(self.fin)
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=20)
+    clases = models.ManyToManyField(Clase)
+    monitores = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.nombre
+
+class Estudiante(models.Model):
+    identificacion = models.CharField(max_length=20)
+    tipo_documento = models.CharField(max_length=30, choices=DOCUMENTOS)
+    nombre = models.CharField(max_length=200)
+    correo = models.CharField(max_length=100)
+    cursos = models.ManyToManyField(Curso)
+
+    def __str__(self):
+        return self.identificacion + " - " + self.nombre
+
+class Asistencia(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    monitor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.curso.nombre + " - " + str(self.fecha)
